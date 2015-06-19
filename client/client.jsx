db37f79362b9092ccf9c91c754c3e31f1e33f99e
Yotta Second <ovaltinevortex@gmail.com>
2015-06-19 00:21:24 -0400

    also thought this stuff was committed, why did I even branch this in the first place?

--- client/client.jsx
+++ client/client.jsx
@@ -1,144 +1,137 @@
-React = require('react');
-var io = require('socket.io-client');
-var socket = io();
+import React from 'react';
+import shouldPureComponentUpdate from 'react-pure-render/function';
+const Draggable = require('react-draggable');
 
-//global objects that aren't going to change often (don't want to send from server constantly)
-synthDefs = {};
-synthDescs = {};
-hardwareBuses = {};
+const io = require('socket.io-client');
+const socket = io();
+React.initializeTouchEvents(true);
 
-//parGroupList -> parGroup -> synthList -> synth
-App = React.createClass({
-  addGroup: function(){
-    socket.emit('addGroup');
-  },
-  render: function() {
-    var createOption = function(val) {
-      return <option value={val}>{val}</option>;
-    }
-    return <div>
-      <button onClick={this.addGroup}>addGroup</button>
-      <select id="synthSelect">
-      {Object.keys(this.props.synthDefs).map(createOption)}
-      </select>
-      <ParGroupList groups={this.props.appProps.parGroupList} />
-      <BusList buses={this.props.appProps.busList} />
-    </div>;
+//@Util
+const renderNode = (node) => {
+  if(node.type == 'group'){
+    // console.log(node);
+    return <Group {...node} id={node.key}></Group>
   }
-});
-
-ParGroupList = React.createClass({
-  render: function() {
-    var createParGroup = function(instance){
-      return <ParGroup instance={instance} key={instance.nodeID} />;
-    }
-    return <div className="row-grid">{this.props.groups.map(createParGroup)}</div>;
+  else if(node.type == 'parGroup'){
+    // console.log(node);
+    return <ParGroup {...node} id={node.key}></ParGroup>
   }
-});
+  else if(node.type == 'synth'){
+    return <Synth {...node} id={node.key}></Synth>
+  }
+}
 
-ParGroup = React.createClass({
-  addSynth: function(){
-    var req = {
-     instance: this.props.instance,
-     synth: document.getElementById('synthSelect').value,
-      //args: ['inBus', 1, 'outBus', 1]
-      args: []
-    }
-    socket.emit('addSynth', req);
-  },
-  removeGroup: function(){
-    socket.emit('removeGroup', this.props.instance);
-  },
-  addGroupAfter: function(){
-    socket.emit('addGroupAfter', this.props.instance);
-  },
-  addGroupBefore: function(){
-    socket.emit('addGroupBefore', this.props.instance);
-  },
-  componentDidMount: function(){
-    // nx.add('dial',
-    //   {parent: this.props.instance.nodeId});
-  },
-  render: function(){
-    return <div className="par-group" id={this.props.instance.nodeId}>
-        Group # {this.props.instance.index}<br/>
-        NodeID: {this.props.instance.nodeId}<br/>
-        <button onClick={this.addGroupBefore}>addGroupBefore</button>
-        <button onClick={this.addSynth}>addSynth</button> 
-        <button onClick={this.removeGroup}>removeGroup</button>
-        <button onClick={this.addGroupAfter}>addGroupAfter</button>
-        <SynthList synths={this.props.instance.synthList} />
+//@Component
+class App extends React.Component{
+  shouldComponentUpdate = shouldPureComponentUpdate;
+  render(){
+    return <Group nodes={this.props.nodes} />
+  }
+}
+
+//@Component
+class Group extends React.Component{
+  shouldComponentUpdate = shouldPureComponentUpdate;
+  handleStart(e, ui){
+    e.stopPropagation();
+  }
+  handleDragStop(e, ui){
+    const req = {id: this.props.id};
+    socket.emit('moveNode', req);
+  }
+  //reset the position of the node, if drag failed
+  resetNodePosition(){
+    console.log('resetting node position');
+    this.refs['theDraggable'].setState({clientX: 0, clientY: 0});
+  }
+  componentDidMount(){
+    console.log('resetNode'+this.props.id);
+    socket.on('resetNode'+this.props.id, this.resetNodePosition.bind(this));
+  }
+  componentWillUnMount(){
+    this.socket.removeListener('resetNode'+this.props.key, this.resetNodePosition);
+  }
+  render(){
+    return (<Draggable onStart={this.handleStart} onStop={this.handleDragStop.bind(this)} ref='theDraggable' zIndex={10000}>
+      <div className='group'>
+        {this.props.nodes.map(renderNode)}
       </div>
+    </Draggable>);
   }
-});
+}
 
-SynthList = React.createClass({
-  render: function(){
-    var createSynth = function(instance){
-      return <Synth instance={instance} key={instance.nodeID} />;
-    }
-    return <div className="column-grid">{this.props.synths.map(createSynth)}</div>;
+//@Component
+class ParGroup extends React.Component{
+  shouldComponentUpdate = shouldPureComponentUpdate.bind(this);
+  handleStart(e, ui){
+    e.stopPropagation();
   }
-});
-
-Synth = React.createClass({
-    componentDidMount: function(){
-        var that = this;
-        var dial = nx.add('dial',
-               {parent: String(this.props.instance.nodeId)});
-        dial.transmit({value: dial.val, name: dial.name});
-        dial.sendsTo(function(data){
-            console.log(data.value);
-            console.log(String(that.props.instance.nodeId))
-            var req = {
-                nodeId: that.props.instance.nodeId,
-                paramName: 'delTime',
-                value: (data.value * 2 + 0.02)
-            };
-            socket.emit('controlSynth', req);
-        });
-    // socket.emit('addSynth', req);
-    },
-  render: function(){
-      return <div className="synth" id={this.props.instance.nodeId}>Synth index: {this.props.instance.index}<br/>
-      NodeId: {this.props.instance.nodeId}
+  handleDragStop(e, ui){
+    const req = {id: this.props.id};
+    socket.emit('moveNode', req);
+  }
+  //reset the position of the node, if drag failed
+  resetNodePosition(){
+    console.log('resetting node position');
+    this.refs['theDraggable'].setState({clientX: 0, clientY: 0});
+  }
+  componentDidMount(){
+    console.log('resetNode'+this.props.id);
+    socket.on('resetNode'+this.props.id, this.resetNodePosition.bind(this));
+  }
+  componentWillUnMount(){
+    this.socket.removeListener('resetNode'+this.props.key, this.resetNodePosition);
+  }
+  render(){
+    return (<Draggable onStart={this.handleStart} onStop={this.handleDragStop.bind(this)} ref='theDraggable' bounds={'parent'} zIndex={10000}>
+    <div className='par-group'>
+      {this.props.nodes.map(renderNode)}
     </div>
+    </Draggable>);
   }
-});
+}
 
-
-BusList = React.createClass({
-  render: function(){
-    var createBus = function(instance){
-      return <Bus instance={instance} />;
-    }
-    return <div>{this.props.buses.map(createBus)}</div>
+//@Component
+class Synth extends React.Component{
+  shouldComponentUpdate = shouldPureComponentUpdate.bind(this);
+  handleStart(e, ui){
+    e.stopPropagation();
   }
-});
-
-Bus = React.createClass({
-  render: function(){
-    return <div>Bus: {this.props.instance}</div>;
+  handleDragStop(e, ui){
+    const req = {id: this.props.id};
+    socket.emit('moveNode', req);
   }
-});
-
-function renderApp(data){
-  React.render(<App appProps={data} synthDefs={synthDefs} synthDescs={synthDescs} hardwareBuses={hardwareBuses} />, document.getElementById("app"));
+  //reset the position of the node, if drag failed
+  resetNodePosition(){
+    console.log('resetting node position');
+    this.refs['theDraggable'].setState({clientX: 0, clientY: 0});
+  }
+  componentDidMount(){
+    console.log('resetNode'+this.props.id);
+    socket.on('resetNode'+this.props.id, this.resetNodePosition.bind(this));
+  }
+  componentWillUnMount(){
+    this.socket.removeListener('resetNode'+this.props.key, this.resetNodePosition);
+  }
+  render(){
+    return (
+      <Draggable onStart={this.handleStart} onStop={this.handleDragStop.bind(this)} ref='theDraggable' zIndex={10000} moveOnStartChange={true}>
+        <span
+          className='synth' 
+          style={{
+            color: '#FFF'
+            }}
+        >
+          Synth{this.props.id}
+        </span>
+      </Draggable>
+    );
+  }
 }
 
-function setSynthDefs(data){
-  synthDefs = data;
+const renderApp = (data) =>{
+  console.log(data);
+  React.render(<App nodes={data.nodes} />, document.getElementById('app'));
 }
 
-function setSynthDescs(data){
-  synthDescs = data;
-}
-
-function setHardwareBuses(data){
-  hardwareBuses = data;
-}
-
-socket.on('synthDefs', setSynthDefs);
-socket.on('synthDescs', setSynthDescs);
-socket.on('hardwareBuses', setHardwareBuses);
 socket.on('appState', renderApp);
