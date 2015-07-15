a80a9d7ffd3f5dfe642ab3777402a501737b3f4d
Yotta Second <ovaltinevortex@gmail.com>
2015-07-14 22:23:40 -0400

    still don't have a great solution for ES6 pure render mixin...

--- client/components/Components.js
+++ client/components/Components.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import shouldPureComponentUpdate from 'react-pure-render/function';
 import { renderNode } from '../Utils';
 import NodeStore from '../stores/NodeStore';
 import NodeActions from '../actions/NodeActions';
