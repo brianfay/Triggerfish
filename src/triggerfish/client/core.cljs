fffe016b5987301f956826728530f874e90d17b8
YottaSecond <ovaltinevortex@gmail.com>
2016-02-17 00:16:54 -0500

    Got patch cables to render consistently
    even if they are ugly and in the wrong place...

--- src/triggerfish/client/core.cljs
+++ src/triggerfish/client/core.cljs
@@ -26,8 +26,9 @@
   (def chsk-state state))
 
 (def initial-state
-  {:patch nil
-   :positions nil
+  {:objects {}
+   :connections {}
+   :positions {}
    })
 
 (register-handler
@@ -39,7 +40,7 @@
 (register-handler
  :patch-recv
  (fn [db [ev-id patch]]
-   (assoc db :patch patch)))
+   (assoc db :objects (dissoc patch :connections) :connections (:connections patch))))
 
 ;;Updates the app-db with the position of an inlet or outlet
 (register-handler
@@ -48,16 +49,22 @@
    (assoc-in db [:positions [obj-id inlet-or-outlet-name]] pos)))
 
 (register-sub
- :patch
+ :objects
  (fn
    [db _]
-   (reaction (:patch @db))))
+   (reaction (:objects @db))))
 
 (register-sub
  :positions
  (fn
    [db _]
    (reaction (:positions @db))))
+
+(register-sub
+ :connections
+ (fn
+   [db _]
+   (reaction (:connections @db))))
 
 (defn request-patch!
   "Sends a request to :patch/notify, which will send out the current patch atom."
@@ -106,63 +113,67 @@
           (sente/start-client-chsk-router!
            ch-chsk event-msg-handler)))
 
-(defn outlet-component
-  [name]
-  [:div {:class "outlet" :ref name}
-   name])
-
-(defn inlet-component
-  [name]
-  [:div {:class "inlet" :ref name}
-   name])
-
-(defn get-ref-node
-  [this ref]
-  (.findDOMNode js/React
-                (goog.object/get (.-refs this) ref)))
-
 (defn get-bounding-rect
   [node]
   (let [rect (.getBoundingClientRect node)]
-    {:bottom (.-bottom rect)
-     :top (.-top rect)
-     :left (.-left rect)
-     :right (.-right rect)}))
+    {:bottom (+ (.-bottom rect) (.-scrollY js/window))
+     :top (+ (.-top rect) (.-scrollY js/window))
+     :left (+ (.-left rect) (.-scrollX js/window))
+     :right (+ (.-right rect) (.-scrollX js/window))}))
+
+(defn outlet-component
+  [id name]
+  (reagent/create-class
+   {
+    :component-did-mount
+    (fn [this]
+      (dispatch [:update-position id name (get-bounding-rect (reagent/dom-node this))]))
+    :reagent-render
+    (fn [id name]
+      [:div {:class "outlet" :key (str id name)}
+       name])}))
+
+(defn inlet-component
+  [id name]
+  (reagent/create-class
+   {
+    :component-did-mount
+    (fn [this]
+      (dispatch [:update-position id name (get-bounding-rect (reagent/dom-node this))]))
+    :reagent-render
+    (fn [id name]
+      [:div {:class "inlet" :key (str id name)}
+       name])}))
 
 (defn object-component
   "Component for a Triggerfish Object."
   [[id obj-map]]
   (let [x-pos (:x-pos obj-map)
         y-pos (:y-pos obj-map)]
-    (reagent/create-class
-     {
-      :component-did-mount
-      (fn [this] 
-        (doall
-         (map
-          (fn
-            [name]
-            (dispatch [:update-position id name
-                            (get-bounding-rect (get-ref-node this (first (keys (:inlets obj-map)))))]))
-          (keys (merge (:inlets obj-map) (:outlets obj-map))))))
-
-      :reagent-render
       (fn [[id obj-map]]
         [:div {:class "object"
                :style {:left x-pos
                        :top  y-pos}}
          [:div (str (:name obj-map))]
-         (map #(with-meta (inlet-component %) {:key (str id %)}) (keys (:inlets obj-map)))
-         (map #(with-meta (outlet-component %) {:key (str id %)}) (keys (:outlets obj-map)))])})))
+         (map (fn [name]
+                ^{:key (str id name)}
+                [inlet-component id name])
+              (keys (:inlets obj-map)))
+         (map (fn [name]
+                ^{:key (str id name)}
+                [outlet-component id name])
+              (keys (:outlets obj-map)))])))
 
 (defn patch-cable
-  [conn positions]
-  (let [[[in-id inlet-name] [out-id outlet-name]] conn
-        x1 (:left (get positions [in-id inlet-name]))
-        y1 (:bottom (get positions [in-id inlet-name]))
-        x2 (:right (get positions [out-id outlet-name]))
-        y2 (:bottom (get positions [out-id outlet-name]))]
+  [conn]
+  (let [positions (subscribe [:positions])
+        [[in-id inlet-name] [out-id outlet-name]] conn
+        x1 (:left (get @positions [in-id inlet-name]))
+        y1 (:bottom (get @positions [in-id inlet-name]))
+        x2 (:right (get @positions [out-id outlet-name]))
+        y2 (:bottom (get @positions [out-id outlet-name]))]
     (fn []
+      (println "rendering patch-cable")
       [:line {:stroke "black"
               :stroke-width 1
               :x1 x1 :y1 y1
@@ -172,23 +183,56 @@
 ;;   [accum [obj-name obj-map]]
 ;;    (assoc accum obj-name (select-keys obj-map [:x-pos :y-pos])))
 
+(defn v-center
+  [position]
+  (/ (+ (:bottom position) (:top position)) 2))
+
+(defn h-center
+  [position]
+  (/ (+ (:left position) (:right position)) 2))
+
+(defn cables-component
+  []
+  (let [connections (subscribe [:connections])
+        positions   (subscribe [:positions])]
+    (fn []
+      ;; (println "rendering cables-comopnent " @positions)
+      [:svg {:class "line-box"}
+       [:g
+        (doall (map (fn [conn]
+                (let [[[in-id inlet-name] [out-id outlet-name]] conn
+                      pos1 (get @positions [in-id inlet-name])
+                      pos2 (get @positions [out-id outlet-name])
+                      x1 (:left pos1)
+                      y1 (:top pos1)
+                      x2 (:right pos2)
+                      y2 (:top pos2)]
+                  ^{:key conn}
+                  [:line {:stroke "white"
+                          :stroke-width 1
+                          :x1 x1 :y1 y1
+                          :x2 x2 :y2 y2}])) @connections))]])))
+
 (defn patch-component
   []
-  (let [patch (subscribe [:patch])
-        objects (reaction (dissoc @patch :connections :positions))
+  (let [objects (subscribe [:objects])
         ;; object-positions (reaction (reduce build-obj-positions {} @objects))
-        positions (subscribe [:positions])
-        connections (reaction (:connections @patch))]
+        ;; positions (subscribe [:positions])
+        ;; connections (subscribe [:connections])]
+        ]
+        ;; connections (reaction (:connections @patch))]
     (fn
       []
       [:div {:id "patch"}
        (map (fn [obj]
               (with-meta [object-component obj]
                 {:key (first obj)})) @objects)
-       [:svg {:class "line-box"}
-        [:g (doall (map (fn [conn]
-                    (with-meta [patch-cable conn @positions]
-                      {:key conn})) @connections))]]])))
+       [cables-component]])))
+       ;; [:svg {:class "line-box"}
+       ;;  [:g (doall (map (fn [conn]
+       ;;              (with-meta [patch-cable conn @positions]
+       ;;                {:key conn})) @connections))]]])))
+
 
 (defn app
   []
